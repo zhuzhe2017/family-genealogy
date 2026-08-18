0d2b86b2dca40a605ce106019b47c99926ad272b
zhuzhe <dingjiekeji@yeah.net>
2026-08-18 19:50:26 +0800

    fix: 提交 web-admin/build 目录，修复服务器 Vite 启动缺失 build 配置

---
+++ web-admin/build/plugins/devtools.ts
@@ -0,0 +1,7 @@
+import VueDevtools from 'vite-plugin-vue-devtools';
+export function setupDevtoolsPlugin(viteEnv: Env.ImportMeta) {
+  const { VITE_DEVTOOLS_LAUNCH_EDITOR } = viteEnv;
+  return VueDevtools({
+    launchEditor: VITE_DEVTOOLS_LAUNCH_EDITOR
+  });
+}
